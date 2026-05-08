5fe322e07df1f80e210a52ac031db71cb907d4cb
bzh <193992116+bz-z@users.noreply.github.com>
2026-05-07 18:56:15 -0700

    Add basic ui
    added basic ui because page was blank.

--- javascript.js
+++ javascript.js
@@ -8,78 +8,20 @@ let winTable = {
     "scissors": "paper"
 }
 
-
-let promptUser = () => {
-    let choice = prompt("Enter your choice for Rock-Paper-Scissors", "")
-    if (choices.includes(choice)) {
-        console.log("Player entered: " + choice.toLowerCase())
-        return choice.toLowerCase()
-    } else {
-        let randomChoice = promptComputer()
-        console.log("Player entered invalid option, Computer played for you: " + randomChoice)
-        return randomChoice
-    }
-    
-}
-
 let promptComputer = () => {
     let computerChoice = choices[Math.floor(Math.random() * 3)]
     return computerChoice
 }
 
 
-let getWinner = (playerChoice, computerChoice) => {
-    if (winTable[playerChoice] == computerChoice) {
-        return "Player"
-    } else if (winTable[computerChoice] == playerChoice) {
-        return "Computer"
-    } else {
-        return "Tie"
-    }
+let game = () => { //called when player chooses a choice
+    
 }
 
-let applyScore = (condition) => {
-    switch (condition) {
-        case "Player":
-            humanScore += 1
-            console.log("Player has earned a score.")
-            break
-        case "Computer":
-            computerScore += 1
-            console.log("Computer has earned a score.")
-            break
-        case "Tie":
-            console.log("Tie, no one got a score")
-            break
-    }
-    printScore()
-}
+let buttonGroup = document.querySelector('.buttonContainer')
 
-let printScore = () => {
-    console.log(humanScore + " - " + computerScore)
-}
+buttonGroup.addEventListener()
 
-
-
-let game = () => {
-    let playerChoice = promptUser()
-    let computerChoice = promptComputer()
-    console.log("Computer entered: " + computerChoice)
-    applyScore(getWinner(playerChoice, computerChoice))
-}
-
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-async function runGames() {
-    while (true) {
-        game()
-
-        console.log("Beginning new game in 3 seconds")
-        await sleep(20000)
-    }
-}
-
-runGames()
 
 
 
