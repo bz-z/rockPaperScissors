96b4a3ca09bccd0c3d627874d0023d263c4bdd3a
bzh <193992116+bz-z@users.noreply.github.com>
2026-05-07 19:29:39 -0700

    implement HTML buttons and logic
    I implemented the logic for the HTML buttons and logic behind the rps.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
+//DOM variables
+const playerChoosing = document.querySelector('.playerChoosing');
+const computerChoosing = document.querySelector('.computerChoosing');
+const computerScoreDOM = document.querySelector('#computerScore')
+const playerScoreDOM = document.querySelector('#playerScore')
 
+//Trackers
 const choices = ["rock", "paper", "scissors"];
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 const winTable = {
     "rock": "scissors",
@@ -8,21 +14,72 @@ const winTable = {
     "scissors": "paper"
 };
 
+const addPlayerScore = () => {
+    playerScore += 1;
+    playerScoreDOM.textContent = `Player Score: ${playerScore}`;
+
+}
+
+
+const addComputerScore = () => {
+    computerScore += 1;
+    computerScoreDOM.textContent = `Computer Score: ${computerScore}`;
+
+}
+
+let getWinner = (playerChoice, computerChoice) => {
+    if (winTable[playerChoice] == computerChoice) {
+        return "Player"
+    } else if (winTable[computerChoice] == playerChoice) {
+        return "Computer"
+    } else {
+        return "Tie"
+    }
+}
+
 const promptComputer = () => { //return "rock, paper, or scissors"
-    let computerChoice = choices[Math.floor(Math.random() * 3)]
+    let computerChoice = choices[Math.floor(Math.random() * 3)];
     return computerChoice;
 };
 
 
 const handleButtonGroup = (event) => {
-    const pressed = event.target.id;
-    
+    const playerChoice = event.target.id;
+    const computerChoice = promptComputer()
+
+    //Changes the display to match choices
+    changeDisplay(playerChoice, playerChoosing);
+    changeDisplay(computerChoice, computerChoosing);
+
+    // "Player", "Tie", or "Computer"
+    const result = getWinner(playerChoice, computerChoice); 
+    handleResult(result);
 }
+
+const handleResult = (result) => {
+    switch (result) {
+        case 'Player':
+            addPlayerScore();
+            printConsole("Player Won.");
+            break;
+        case 'Computer':
+            addComputerScore();
+            printConsole("Computer Won.");
+            break;
+        case 'Tie':
+            printConsole("Tie.");
+    }
+}
+
+
+
+const changeDisplay = (str, doc) => {
+    doc.textContent = str;
+}
+
 
 const buttonGroup = document.querySelector('.buttonContainer');
 buttonGroup.addEventListener("click", handleButtonGroup);
-
-
 
 const consoleDisplay = document.querySelector('.consoleDisplay');
 const printConsole = (msg) => {
